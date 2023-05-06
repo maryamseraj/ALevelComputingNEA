cf755cfd7650b12272472deced3c12d109b44941
Mary <maryamseraj90@gmail.com>
2023-05-06 16:19:11 +0100

    Update sql.cs

--- Models/sql.cs
+++ Models/sql.cs
@@ -5,24 +5,23 @@ namespace COMPUTINGNEA.Models
 {
 	public class sql
 	{
-        // Storing connection string as a public variable to be accessed elsewhere in the program
-        private string constring;
-        public string Constring
-        {
-            get { return constring; }
-            set { constring = value; }
-        }
+            // Storing connection string as a public variable to be accessed elsewhere in the program
+            private string constring;
+            public string Constring
+            {
+               get { return constring; }
+               set { constring = value; }
+            }
 
-        // defining connection string
-        public sql()
-		{
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "investmentnea.database.windows.net";
-            builder.UserID = "maryamseraj";
-            builder.Password = "************************"; 
-            builder.InitialCatalog = "investmentcalc";
-
-            constring = builder.ConnectionString;
-        }
+            // defining connection string
+            public sql()
+	       {
+		  SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+		  builder.DataSource = "investmentnea.database.windows.net";
+		  builder.UserID = "maryamseraj";
+		  builder.Password = "************************"; 
+		  builder.InitialCatalog = "investmentcalc";
+		  constring = builder.ConnectionString;
+	       }
 	}
 }
